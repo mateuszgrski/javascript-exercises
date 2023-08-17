a538f3e64644cd830ca7132dd737e7a74ff2dc78
Mateusz Grabowski <mateusz.grabowski@bildungsinnovator.com>
2023-08-17 16:46:40 +0200

    Add 2023-08-17

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfRepeats) {
+    if (numberOfRepeats < 0) return "ERROR"
+        let outputString = ""
+        for (let i = 1; i <= numberOfRepeats; i++){
+            outputString = outputString.concat(string)
 
+        }
+        return outputString
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    array.forEach(
+        (arrayElement) => {
+        if (args.includes(arrayElement)){
+            array.splice(array.indexOf(arrayElement), 1)
+            console.log(array)
+        }
+    })
+    return array
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
