99407758bd8c0a36d018737c36ecc0ad17fff28d
Mateusz Grabowski <mateusz.grabowski@bildungsinnovator.com>
2023-08-24 16:39:28 +0200

    Add 2023-08-24

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    return arr.reduce((acc, val) => acc + val, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    return arr.reduce((acc, val) => acc * val)
 };
 
-const power = function() {
-	
+const power = function(basis, exponent) {
+    return basis ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+    n = (n === 0) ? 1 : n * factorial(n-1)
+    return n
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    strWordsNumOnly = str.toLowerCase().replace(/[^a-z0-9]/g, "").split(" ").join("")
+    return strWordsNumOnly === strWordsNumOnly.split("").reverse().join("")
 };
 
 // Do not edit below this line
